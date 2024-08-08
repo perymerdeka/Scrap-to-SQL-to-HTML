b57b9f97e4a930afd8030150d7d1d41899e92392
Yogitestcode <ftgyogi@gmail.com>
2024-08-08 09:09:12 +0700

    login register

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,8 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('Register')
